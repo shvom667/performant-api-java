f5928f0c97ab7c9857abf5e6a35ba1258ba9c9bc
shivom <shivom8787butterbus@gmail.com>
2024-10-13 21:54:18 +0530

    Initial Commit

---
+++ CounterHttpServer.java
@@ -0,0 +1,32 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class CounterHttpServer {
+    // Global counter
+    static int requestCount = 0;
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on port 8080");
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            requestCount++;
+
+            String response = "Counter: " + requestCount;
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
